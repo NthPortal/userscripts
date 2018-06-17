3735d8e622a464c234f4e5bcc22b4413c198f2e3
Nth <7505383+NthPortal@users.noreply.github.com>
2018-06-17 16:58:28 -0400

    Add update metadata to feature-less-youtube.user.js

--- scripts/feature-less-youtube.user.js
+++ scripts/feature-less-youtube.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         feature-less YouTube
 // @namespace    https://nthportal.com
-// @license      Apache License 2.0
-// @version      0.1.0
-// @description  Removes `&feature=___` from YouTube URLs
 // @author       NthPortal
+// @license      Apache License 2.0
+// @description  Removes `&feature=___` from YouTube URLs
+// @version      0.1.1
+// @updateURL    https://github.com/NthPortal/userscripts/raw/master/scripts/feature-less-youtube.user.js
+// @downloadURL  https://github.com/NthPortal/userscripts/raw/master/scripts/feature-less-youtube.user.js
 // @match        https://www.youtube.com/watch*
-// @run-at document-start
+// @run-at       document-start
 // ==/UserScript==
 
 if (window.self === window.top && window.location.search.includes('&feature=')) {
