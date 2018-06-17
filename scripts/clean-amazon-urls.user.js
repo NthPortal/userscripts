d30a0c6710acb6a8b1394fa14e7613c1abbe4426
Nth <7505383+NthPortal@users.noreply.github.com>
2018-06-17 17:00:13 -0400

    Add update metadata to clean-amazon-urls.user.js

--- scripts/clean-amazon-urls.user.js
+++ scripts/clean-amazon-urls.user.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name         Clean Amazon URLs
 // @namespace    https://nthportal.com
-// @license      Apache License 2.0
-// @version      0.1.0
-// @description  Removes unnecessary stuff from the end of Amazon product URLs
 // @author       NthPortal
+// @license      Apache License 2.0
+// @description  Removes unnecessary stuff from the end of Amazon product URLs
+// @version      0.1.1
+// @updateURL    https://github.com/NthPortal/userscripts/raw/master/scripts/clean-amazon-urls.user.js
+// @downloadURL  https://github.com/NthPortal/userscripts/raw/master/scripts/clean-amazon-urls.user.js
 // @match        http://www.amazon.com/*
 // @match        https://www.amazon.com/*
 // @match        http://smile.amazon.com/*
 // @match        https://smile.amazon.com/*
-// @run-at document-start
+// @run-at       document-start
 // ==/UserScript==
 
 if (window.self === window.top && window.location.pathname.match(/\/[^/]*\/dp\/[^/]*\/ref=/)) {
