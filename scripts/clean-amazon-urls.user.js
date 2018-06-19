b091e923c80b228949f4877e5e5219405273b617
Nth <7505383+NthPortal@users.noreply.github.com>
2018-06-19 19:40:12 -0400

    Update clean-amazon-urls.user.js

--- scripts/clean-amazon-urls.user.js
+++ scripts/clean-amazon-urls.user.js
@@ -4,18 +4,25 @@
 // @author       NthPortal
 // @license      Apache License 2.0
 // @description  Removes unnecessary stuff from the end of Amazon product URLs
-// @version      0.1.1
+// @version      0.2.0
 // @updateURL    https://github.com/NthPortal/userscripts/raw/master/scripts/clean-amazon-urls.user.js
 // @downloadURL  https://github.com/NthPortal/userscripts/raw/master/scripts/clean-amazon-urls.user.js
-// @match        http://www.amazon.com/*
-// @match        https://www.amazon.com/*
-// @match        http://smile.amazon.com/*
-// @match        https://smile.amazon.com/*
+// @match        http://www.amazon.com/*/dp/*
+// @match        https://www.amazon.com/*/dp/*
+// @match        http://smile.amazon.com/*dp/*
+// @match        https://smile.amazon.com/*/dp/*
+// @match        http://www.amazon.co.uk/*/dp/*
+// @match        https://www.amazon.co.uk/*/dp/*
+// @match        http://smile.amazon.co.uk/*/dp/*
+// @match        https://smile.amazon.co.uk/*/dp/*
 // @run-at       document-start
 // ==/UserScript==
 
-if (window.self === window.top && window.location.pathname.match(/\/[^/]*\/dp\/[^/]*\/ref=/)) {
+if (window.self === window.top
+    && (window.location.pathname.match(/^\/[^/]*\/dp\/[^/]*\/ref=/)
+       || window.location.search.startsWith('?pf_rd')
+       || window.location.search.includes('&pf_rd'))) {
     var newPath = window.location.pathname.replace(/\/ref=.*$/, '');
-    var newURL = 'https://' + window.location.host + newPath;
+    var newURL = 'https://' + window.location.host + newPath + window.location.hash; // skip search
     window.location.replace(newURL);
 }
