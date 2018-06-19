0c16a309c675c13e682c84864140de4d596e9768
Nth <7505383+NthPortal@users.noreply.github.com>
2018-06-19 19:43:33 -0400

    Update feature-less-youtube.user.js

--- scripts/feature-less-youtube.user.js
+++ scripts/feature-less-youtube.user.js
@@ -4,14 +4,17 @@
 // @author       NthPortal
 // @license      Apache License 2.0
 // @description  Removes `&feature=___` from YouTube URLs
-// @version      0.1.1
+// @version      0.1.2
 // @updateURL    https://github.com/NthPortal/userscripts/raw/master/scripts/feature-less-youtube.user.js
 // @downloadURL  https://github.com/NthPortal/userscripts/raw/master/scripts/feature-less-youtube.user.js
+// @match        http://www.youtube.com/watch*
 // @match        https://www.youtube.com/watch*
 // @run-at       document-start
 // ==/UserScript==
 
-if (window.self === window.top && window.location.search.includes('&feature=')) {
+if (window.self === window.top
+    && (window.location.search.includes('&feature=')
+        || window.location.search.startsWith('?feature='))) {
     var newSearch = window.location.search.replace(/&feature=[^&]*/, '');
     var newURL = 'https://' + window.location.host + window.location.pathname + newSearch + window.location.hash;
     window.location.replace(newURL);
