c70704192228caafa82744e0b6474d7763ddbe54
Nth <7505383+NthPortal@users.noreply.github.com>
2018-06-21 15:43:23 -0400

    Update clean-amazon-urls.user.js

--- scripts/clean-amazon-urls.user.js
+++ scripts/clean-amazon-urls.user.js
@@ -4,7 +4,7 @@
 // @author       NthPortal
 // @license      Apache License 2.0
 // @description  Removes unnecessary stuff from the end of Amazon product URLs
-// @version      0.2.2
+// @version      0.3.0
 // @updateURL    https://github.com/NthPortal/userscripts/raw/master/scripts/clean-amazon-urls.user.js
 // @downloadURL  https://github.com/NthPortal/userscripts/raw/master/scripts/clean-amazon-urls.user.js
 // @match        http://www.amazon.com/*/dp/*
@@ -18,13 +18,15 @@
 // @run-at       document-start
 // ==/UserScript==
 
-if (window.self === window.top
-    && (window.location.pathname.match(/^\/[^/]*\/dp\/[^/]*\/ref=/)
-        || window.location.search.startsWith('?pf_rd')
-        || window.location.search.includes('&pf_rd'))) {
-    var newPath = window.location.pathname.replace(/\/ref=.*$/, '');
-    var newURL = 'https://' + window.location.host + newPath + window.location.hash; // skip search
-    if (newURL !== window.location.href) {
-        window.location.replace(newURL);
+(function() {
+    if (window.self === window.top
+        && (window.location.pathname.match(/^\/[^/]*\/dp\/[^/]*\/ref=/)
+            || window.location.search.startsWith('?pf_rd')
+            || window.location.search.includes('&pf_rd'))) {
+        var newPath = window.location.pathname.replace(/\/ref=.*$/, '');
+        var newURL = 'https://' + window.location.host + newPath + window.location.hash; // skip search
+        if (newURL !== window.location.href) {
+            window.location.replace(newURL);
+        }
     }
-}
+})();
