546c021c02f7c9adf1b94356d7eee98cd30c3662
Nth <7505383+NthPortal@users.noreply.github.com>
2018-06-21 15:48:55 -0400

    Update no-ugly-analytics-aggressive.user.js

--- scripts/no-ugly-analytics-aggressive.user.js
+++ scripts/no-ugly-analytics-aggressive.user.js
@@ -4,7 +4,7 @@
 // @author       NthPortal
 // @license      Apache License 2.0
 // @description  Removes `[&?]utm_` and other analytics junk from URLs
-// @version      0.2.0
+// @version      0.3.0
 // @updateURL    https://github.com/NthPortal/userscripts/raw/master/scripts/no-ugly-analytics-aggressive.user.js
 // @downloadURL  https://github.com/NthPortal/userscripts/raw/master/scripts/no-ugly-analytics-aggressive.user.js
 // @match        http://*/*
@@ -18,16 +18,19 @@
  * This script is very aggressive and may rewrite URLs *
  * that you don't want it to. Use it at your own risk. *
 \*******************************************************/
-if (window.self === window.top
-    && (window.location.search.includes('&utm_')
-        || window.location.search.startsWith('?utm_'))) {
-    // Fix search
-    var newSearch = window.location.search.replace(/&utm_[^&=]*=[^&]*/g, ''); // removes all but a leading parameter
-        newSearch = newSearch.replace(/utm_[^&=]*=[^&]*&/, '');               //  removes a leading parameter IF there are other parameters after it; this preserves the '?' for them
-        newSearch = newSearch.match(/^\?utm_[^&=]*=[^&]*/) ? '' : newSearch;  // removes a leading parameter IF there are no other parameters
-    // can't force https, cuz we don't know what site it is
-    var newURL = window.location.protocol + '//' + window.location.host + window.location.pathname + newSearch + window.location.hash;
-    if (newURL !== window.location.href) {
-        window.location.replace(newURL);
+(function() {
+    if (window.self === window.top
+        && (window.location.search.includes('&utm_')
+            || window.location.search.startsWith('?utm_'))) {
+        // Fix search
+        var newSearch = window.location.search.replace(/&utm_[^&=]*=[^&]*/g, ''); // removes all but a leading parameter
+            newSearch = newSearch.replace(/utm_[^&=]*=[^&]*&/, '');               //  removes a leading parameter IF there are other parameters after it; this preserves the '?' for them
+            newSearch = newSearch.match(/^\?utm_[^&=]*=[^&]*/) ? '' : newSearch;  // removes a leading parameter IF there are no other parameters
+        // can't force https, cuz we don't know what site it is
+        var newURL = window.location.protocol + '//' + window.location.host + window.location.pathname + newSearch + window.location.hash;
+        if (newURL !== window.location.href) {
+            window.location.replace(newURL);
+        }
     }
-}
+})();
+
