4c006cc05be17b1429fc15add0f1f1e358619b58
Nth <7505383+NthPortal@users.noreply.github.com>
2018-06-24 16:43:43 -0400

    Update clean-amazon-urls.user.js

--- scripts/clean-amazon-urls.user.js
+++ scripts/clean-amazon-urls.user.js
@@ -4,26 +4,33 @@
 // @author       NthPortal
 // @license      Apache License 2.0
 // @description  Removes unnecessary stuff from the end of Amazon product URLs
-// @version      0.3.0
+// @version      0.4.0
 // @updateURL    https://github.com/NthPortal/userscripts/raw/master/scripts/clean-amazon-urls.user.js
 // @downloadURL  https://github.com/NthPortal/userscripts/raw/master/scripts/clean-amazon-urls.user.js
-// @match        http://www.amazon.com/*/dp/*
+// @match        https://www.amazon.com/dp/*
 // @match        https://www.amazon.com/*/dp/*
-// @match        http://smile.amazon.com/*dp/*
+// @match        https://www.amazon.com/gp/product/*
+// @match        https://smile.amazon.com/dp/*
 // @match        https://smile.amazon.com/*/dp/*
-// @match        http://www.amazon.co.uk/*/dp/*
+// @match        https://smile.amazon.com/gp/product/*
+// @match        https://www.amazon.co.uk/dp/*
 // @match        https://www.amazon.co.uk/*/dp/*
-// @match        http://smile.amazon.co.uk/*/dp/*
+// @match        https://www.amazon.co.uk/gp/product/*
+// @match        https://smile.amazon.co.uk/dp/*
 // @match        https://smile.amazon.co.uk/*/dp/*
+// @match        https://smile.amazon.co.uk/gp/product/*
 // @run-at       document-start
 // ==/UserScript==
 
 (function() {
     if (window.self === window.top
-        && (window.location.pathname.match(/^\/[^/]*\/dp\/[^/]*\/ref=/)
+        && (window.location.pathname.match(/^(\/[^/]*)?\/dp\/[^/]*\/ref=/)
+            || window.location.pathname.match(/^\/gp\/product\/[^/]*\/ref=/)
             || window.location.search.startsWith('?pf_rd')
             || window.location.search.includes('&pf_rd'))) {
         var newPath = window.location.pathname.replace(/\/ref=.*$/, '');
+            newPath = newPath.replace(/^\/[^/]*\/dp/, '/dp');
+            newPath = newPath.replace(/^\/gp\/product/, '/dp');
         var newURL = 'https://' + window.location.host + newPath + window.location.hash; // skip search
         if (newURL !== window.location.href) {
             window.location.replace(newURL);
