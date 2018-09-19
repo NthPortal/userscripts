aa51b081b06422a0df6d672dd3df9fa36e278d19
Nth <7505383+NthPortal@users.noreply.github.com>
2018-09-18 23:05:10 -0400

    Update clean-etsy-urls.user.js
    Fixes #1

--- scripts/clean-etsy-urls.user.js
+++ scripts/clean-etsy-urls.user.js
@@ -4,7 +4,7 @@
 // @author       NthPortal
 // @license      Apache License 2.0
 // @description  Removes unnecessary stuff from the end of Etsy product URLs
-// @version      0.1.1
+// @version      0.2.0
 // @updateURL    https://github.com/NthPortal/userscripts/raw/master/scripts/clean-etsy-urls.user.js
 // @downloadURL  https://github.com/NthPortal/userscripts/raw/master/scripts/clean-etsy-urls.user.js
 // @match        http://www.etsy.com/listing/*/*
@@ -15,10 +15,15 @@
 
 (function() {
     if (window.location.search.startsWith('?ref=')
-        || window.location.search.includes('&ref=')) {
-        var newSearch = window.location.search.replace(/&ref=[^&]*/, ''); // removes non-leading parameter
-            newSearch = newSearch.replace(/ref=[^&]*&/, '');              //  removes a leading parameter IF there are other parameters after it; this preserves the '?' for them
-            newSearch = newSearch.match(/^\?ref=[^&]*/) ? '' : newSearch; // removes a leading parameter IF there are no other parameters
+        || window.location.search.startsWith('?ga_')
+        || window.location.search.includes('&ref=')
+        || window.location.search.includes('&ga_')) {
+        var newSearch = window.location.search.replace(/&ref=[^&]*/, ''); // removes non-leading parameter `ref`
+            newSearch = newSearch.replace(/ref=[^&]*&/, '');              // removes a leading parameter `ref` IF there are other parameters after it; this preserves the '?' for them
+            newSearch = newSearch.match(/^\?ref=[^&]*/) ? '' : newSearch; // removes a leading parameter `ref` IF there are no other parameters
+            newSearch = window.location.search.replace(/&ga_[^&=]*=[^&]*/, ''); // removes non-leading parameter `ga_*`
+            newSearch = newSearch.replace(/ga_[^&=]*=[^&]*&/, '');              // removes a leading parameter `ga_*` IF there are other parameters after it; this preserves the '?' for them
+            newSearch = newSearch.match(/^\?ga_[^&=]*=[^&]*/) ? '' : newSearch; // removes a leading parameter `ga_*` IF there are no other parameters
         var newURL = 'https://' + window.location.host + window.location.pathname + newSearch + window.location.hash;
         if (newURL !== window.location.href) {
             window.location.replace(newURL);
