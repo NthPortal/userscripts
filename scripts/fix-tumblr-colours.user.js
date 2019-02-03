ed19a61c4f8d93e76ed89fa2e6227e43afe16c3b
Nth <7505383+NthPortal@users.noreply.github.com>
2019-02-03 17:26:13 -0500

    Update fix-tumblr-colours.user.js

--- scripts/fix-tumblr-colours.user.js
+++ scripts/fix-tumblr-colours.user.js
@@ -4,7 +4,7 @@
 // @author       NthPortal
 // @license      Apache License 2.0
 // @description  Use the old Tumblr background colour rather than the garbage new one
-// @version      0.1.0
+// @version      0.2.0
 // @updateURL    https://github.com/NthPortal/userscripts/raw/master/scripts/fix-tumblr-colours.user.js
 // @downloadURL  https://github.com/NthPortal/userscripts/raw/master/scripts/fix-tumblr-colours.user.js
 // @match        *://www.tumblr.com/*
@@ -12,5 +12,12 @@
 // ==/UserScript==
 
 (function() {
-    document.body.classList.remove("flag--accessibility-design-update"); // colour for reference #36465D
+    function fixStyle() {
+        document.body.classList.remove("flag--accessibility-design-update"); // colour for reference #36465D
+    }
+    function fixRepeatedly(times) {
+        fixStyle();
+        if (times > 0) setTimeout(() => fixRepeatedly(times - 1), 10);
+    }
+    fixRepeatedly(10);
 })();
