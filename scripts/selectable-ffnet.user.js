d6dfb0374156f01ab4ddb050ec8e30778bf7cfdd
Princess | April <7505383+NthPortal@users.noreply.github.com>
2022-01-16 20:12:54 -0500

    Fix typo in slectable-ffnet.user.js name

--- scripts/selectable-ffnet.user.js
+++ scripts/selectable-ffnet.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Selectable FFnet
+// @name         Selectable FFNet
 // @namespace    https://nthportal.com
 // @author       NthPortal
 // @license      Apache License 2.0
 // @description  Allows text selection on FanFiction.net
-// @version      0.1.0
+// @version      0.1.1
 // @updateURL    https://github.com/NthPortal/userscripts/raw/master/scripts/selectable-ffnet.user.js
 // @downloadURL  https://github.com/NthPortal/userscripts/raw/master/scripts/selectable-ffnet.user.js
 // @match        *://www.fanfiction.net*
