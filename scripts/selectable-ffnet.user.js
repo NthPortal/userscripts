0512b31164fed02a23e1326128e1e3d99f7e37ea
Princess | April <7505383+NthPortal@users.noreply.github.com>
2022-01-16 20:22:34 -0500

    Update selectable-ffnet.user.js

--- scripts/selectable-ffnet.user.js
+++ scripts/selectable-ffnet.user.js
@@ -4,16 +4,21 @@
 // @author       NthPortal
 // @license      Apache License 2.0
 // @description  Allows text selection on FanFiction.net
-// @version      0.1.1
+// @version      0.2.0
 // @updateURL    https://github.com/NthPortal/userscripts/raw/master/scripts/selectable-ffnet.user.js
 // @downloadURL  https://github.com/NthPortal/userscripts/raw/master/scripts/selectable-ffnet.user.js
-// @match        *://www.fanfiction.net*
+// @match        *://www.fanfiction.net/s/*/*/*
 // @noframes
-// @run-at       document-start
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
-    for (var elem of document.getElementsByClassName('storytextp')) {
-        elem.style.removeProperty('user-select')
+    function allow_text_selection() {
+        for (var elem of document.getElementsByClassName('storytextp')) {
+            console.log(elem);
+            console.log(elem.style.removeProperty('user-select'));
+            console.log(elem);
+        }
     }
+    window.setTimeout(allow_text_selection, 1000);
 })();
