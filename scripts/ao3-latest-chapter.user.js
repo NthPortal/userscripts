c7563d3fdde6e13ce42110b0f9349f1a9e76e423
Marissa | April <7505383+NthPortal@users.noreply.github.com>
2024-07-17 19:23:19 -0400

    Update ao3-latest-chapter.user.js

--- scripts/ao3-latest-chapter.user.js
+++ scripts/ao3-latest-chapter.user.js
@@ -4,7 +4,7 @@
 // @author       Marissa
 // @license      Apache License 2.0
 // @description  Adds a latest chapter button to the top navigation.
-// @version      0.2.7
+// @version      0.3.0
 // @updateURL    https://github.com/NthPortal/userscripts/raw/master/scripts/ao3-latest-chapter.user.js
 // @downloadURL  https://github.com/NthPortal/userscripts/raw/master/scripts/ao3-latest-chapter.user.js
 // @match        https://archiveofourown.org/*
@@ -14,32 +14,19 @@
 (function() {
     const match = location.pathname.match(/^(\/works\/\d+\/chapters\/)\d+/);
 
-    if (match) {
+    if (match && (document.querySelector('li.chapter.next') !== null)) {
+        const chapters = document.getElementById('selected_id').children;
+        const lastChapterId = chapters[chapters.length - 1].value;
+
         const link = document.createElement('a');
-        link.style.pointerEvents = 'none';
         link.appendChild(document.createTextNode('Latest Chapter ' + String.fromCharCode(0x2192) + '|'));
+        link.href = match[1] + lastChapterId;
 
         const buttonParent = document.createElement('li');
         buttonParent.className = 'chapter';
-        buttonParent.ariaDisabled = true;
-        buttonParent.style.cursor = 'not-allowed';
         buttonParent.appendChild(link);
 
         const chaptersParent = document.getElementById('chapter_index').parentElement;
         chaptersParent.parentElement.insertBefore(buttonParent, chaptersParent);
-
-        function maybe_update_button() {
-            const chapters = document.getElementById('selected_id').children;
-            const lastChapter = chapters[chapters.length-1];
-            if (!lastChapter.selected) {
-                const lastChapterId = lastChapter.value;
-                link.href = match[1] + lastChapterId;
-                link.style.pointerEvents = 'auto';
-                buttonParent.ariaDisabled = false;
-                buttonParent.style.cursor = 'auto';
-            }
-
-        }
-        window.setTimeout(maybe_update_button, 1000);
     }
 })();
